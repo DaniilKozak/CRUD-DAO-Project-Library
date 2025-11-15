6c6184a00d610c333ddec4d4c3e0cae8e6f56b1f
Daniil <DDCBrazl@yandex.ru>
2025-11-15 15:45:45 +0500

    Added content at ConnectionManager, PropertiesUtil, application.properties

--- src/com/babaev/jdbc/starter/util/ConnectionManager.java
+++ src/com/babaev/jdbc/starter/util/ConnectionManager.java
@@ -3,4 +3,8 @@ package com.babaev.jdbc.starter.util;
 public final class ConnectionManager {
     private ConnectionManager() {
     }
+    private static final String URL_KEY = "db.url";
+    private static final String USERNAME_KEY = "db.username";
+    private static final String PASSWORD_KEY = "db.password";
+    private static final String POOL_SIZE_KEY = "db.pool.size";
 }
